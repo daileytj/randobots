bb84dc27d707e94aa3e5753fb24371ec487a2265
daileytj <daileytj89@gmail.com>
2021-08-14 10:00:06 -0400

    Scaffold new react app

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+/**
+ Copyright (c) 2021-present, Eaton
+
+ All rights reserved.
+
+ This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+ **/
+import 'react-app-polyfill/stable';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CssBaseline, ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
+import * as PXBThemes from '@pxblue/react-themes';
+import '@pxblue/react-themes/open-sans';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+ReactDOM.render(
+    // <React.StrictMode>
+        <ThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
+            <BrowserRouter>
+                <CssBaseline />
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>,
+    // </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
